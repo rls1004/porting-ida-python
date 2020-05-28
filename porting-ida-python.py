b64adaa93dd6f405f145b099da89a2a6a9e1d2b5
MinJeong Kim <98nba@naver.com>
2020-05-28 02:52:52 +0000

    Fix an issue where function porting was done only once.

--- porting-ida-python.py
+++ porting-ida-python.py
@@ -18,42 +18,45 @@ def main():
   
   for i in range(len(ida6)):
     target = '.'+ida6[i].split('.')[-1]
-    cnt = data.count(target.split('(')[0])
-    if cnt > 0:
-      if '(' in target:
-        replace_data = ida7[i]
-        before_argv = target.split('(')[1].split(')')[0].split(',')
-        before_argc = len(before_argv)
-        after_argv = replace_data.split('(')[1].split(')')[0].split(',')
-        after_argc = len(after_argv)
-        argvTo = [-1 for _ in range(after_argc)]
-        for i in range(before_argc):
-          try:
-            argvTo[after_argv.index(before_argv[i])] = i
-          except:
-            continue
-        replace_data = '.'+replace_data.split('.')[1].split('(')[0]+'('
+    while True:
+      cnt = data.count(target.split('(')[0])
+      if cnt > 0:
+        if '(' in target:
+          replace_data = ida7[i]
+          before_argv = target.split('(')[1].split(')')[0].split(',')
+          before_argc = len(before_argv)
+          after_argv = replace_data.split('(')[1].split(')')[0].split(',')
+          after_argc = len(after_argv)
+          argvTo = [-1 for _ in range(after_argc)]
+          for j in range(before_argc):
+            try:
+              argvTo[after_argv.index(before_argv[j])] = j
+            except:
+              continue
+          replace_data = '.'+replace_data.split('.')[1].split('(')[0]+'('
 
-        real_code = data[data.index(target.split('(')[0]):]
-        real_code = real_code.split(')')[0]+')'
-        real_argv = real_code.split('(')[1].split(')')[0].split(',')
+          real_code = data[data.index(target.split('(')[0]):]
+          real_code = real_code.split(')')[0]+')'
+          real_argv = real_code.split('(')[1].split(')')[0].split(',')
         
-        for i in range(after_argc):
-          if i > 0: replace_data += ', '
-          if argvTo[i] != -1:
-            replace_data += real_argv[argvTo[i]]
-          else:
-            replace_data += after_argv[i]
+          for j in range(after_argc):
+            if j > 0: replace_data += ', '
+            if argvTo[j] != -1:
+              replace_data += real_argv[argvTo[j]]
+            else:
+              replace_data += after_argv[j]
 
-        replace_data += ')'
-        print(real_code+' -> '+replace_data)
-        data = data.replace(real_code, replace_data, cnt)
+          replace_data += ')'
+          print(real_code+' -> '+replace_data)
+          data = data.replace(real_code, replace_data, cnt)
+        else:
+          print(target+' -> '+'.'+ida7[i].split('.')[-1])
+          data = data.replace(target, '.'+ida7[i].split('.')[-1], cnt)
       else:
-        print(target+' -> '+'.'+ida7[i].split('.')[-1])
-        data = data.replace(target, '.'+ida7[i].split('.')[-1], cnt)
+        break
 
   open('ida7_'+fname, 'w').write(data)
-  print('create: ida7_'+fname)
+  print('[+] create: ida7_'+fname)
       
 
 if __name__ == '__main__':
